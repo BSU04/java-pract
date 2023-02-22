08e8d887ff026894cbbb15bd72b99b4fd4c0a37d
Coderaze07 <93376652+Coderaze07@users.noreply.github.com>
2023-02-22 15:37:56 +0530

    eclipse

---
+++ TCP/Dbconnect.java
@@ -0,0 +1,41 @@
+package dbproj;
+
+import java.sql.*;
+
+public class Dbonnect {
+
+public static void main(String args[]){
+
+try{
+
+//step1 load the driver class
+
+Class.forName("oracle.jdbc.driver.OracleDriver");
+
+//step2 create the connection object
+
+java.sql.Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","cmi");
+
+//step3 create the statement object
+
+Statement stmt=con.createStatement();
+
+//step4 execute query
+
+ResultSet rs=stmt.executeQuery("select * from student");
+
+while(rs.next())
+
+System.out.println(rs.getInt(1)+" "+rs.getString(2));
+
+System.out.println("done");
+
+//step5 close the connection object
+
+con.close();
+
+}catch(Exception e){ System.out.println(e);}
+
+}
+
+}
